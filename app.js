c32dad60579f15a2b90c3917836d863fc5fc4e93
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-06-10 17:39:47 +0530

    add schema

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
-const graphqlHttp = require("express-graphql");
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    // resolver is a function that takes the request and returns a response
+    rootValue: {
+        events: () => {
+            return ["Eat", "Sleep", "Code", "repeat"];
+        },
+        createEvent: (args) => {
+            return args.name;
+        }
+    },
+    graphiql: true
+}));
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
