3b5c998f917459ea5a500cea1ab2a1c6ac2a3cb5
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-06-16 18:23:55 +0530

    topic 4

--- app.js
+++ app.js
@@ -7,14 +7,31 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 
+const events = [];
+
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -25,10 +42,19 @@ app.use('/graphql', graphqlHTTP({
     // resolver is a function that takes the request and returns a response
     rootValue: {
         events: () => {
-            return ["Eat", "Sleep", "Code", "repeat"];
+            return events;
         },
         createEvent: (args) => {
-            return args.name;
+            const event = {
+                _id: Math.random.toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event);
+            console.log(args);
+            return event;
         }
     },
     graphiql: true
